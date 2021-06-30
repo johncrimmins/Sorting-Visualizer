e1c0d8057e4fc5ee71b3be0b46bda4452eb447df
johncrimmins <john.crimmins@protonmail.com>
2021-06-29 23:33:07 -0400

    Added mergesort.

--- sorting-visualizer/src/App.js
+++ sorting-visualizer/src/App.js
@@ -9,6 +9,35 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+function merge(left, right) {
+  let sortedArr = []; // the sorted elements will go here
+
+  while (left.length && right.length) {
+    // insert the smallest element to the sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  // use spread operator and create a new array, combining the three arrays
+  return [...sortedArr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+  const half = arr.length / 2;
+
+  // the base case is array length <=1
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const left = arr.splice(0, half); // the first half of the array
+  const right = arr;
+  return merge(mergeSort(left), mergeSort(right));
+}
+
 const draw = context => {
 
 
@@ -30,6 +59,17 @@ const draw = context => {
   for (var i = 0; i < xPositions.length; i++) {
     context.fillRect(xPositions[i], 0, 10, yHeights[i])
   }
+
+  context.clearRect(0,0,800, 500)
+
+  yHeights = mergeSort(yHeights)
+
+  for (var i = 0; i < xPositions.length; i++) {
+    context.fillRect(xPositions[i], 0, 10, yHeights[i])
+  }
+
+  console.log(yHeights)
+
 };
 
 function App() {
